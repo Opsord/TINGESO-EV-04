f449f719f9cf83168bb43cb4ab63f9bb835485aa
Andres Zelaya <andres.zelaya.d@hotmail.com>
2023-12-17 03:10:29 -0300

    Project initialization

---
+++ ELOAM-App/management-service/src/test/java/ELOAM/managementservice/ManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ELOAM.managementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
